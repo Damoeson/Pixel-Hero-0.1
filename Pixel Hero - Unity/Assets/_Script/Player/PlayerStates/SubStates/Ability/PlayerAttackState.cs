d158c1013a4698f46ff4286ffc277fa4e063c93a
Damoeson <131646917+Damoeson@users.noreply.github.com>
2023-04-27 14:42:35 +0200

    Weapon Setup
    Mise en place du nouveau système d'arme

--- Pixel Hero - Unity/Assets/_Script/Player/PlayerStates/SubStates/Ability/PlayerAttackState.cs
+++ Pixel Hero - Unity/Assets/_Script/Player/PlayerStates/SubStates/Ability/PlayerAttackState.cs
@@ -1,7 +1,18 @@
+using Damoeson.Weapons;
+
 public class PlayerAttackState : PlayerAbilityState
 {
+    private Weapon weapon;
 
-    public PlayerAttackState(Player player, PlayerStateMachine stateMachine, PlayerData playerData, string animBoolName) : base(player, stateMachine, playerData, animBoolName)
+    public PlayerAttackState(Player player, PlayerStateMachine stateMachine, PlayerData playerData, string animBoolName, Weapon weapon) : base(player, stateMachine, playerData, animBoolName)
     {
+        this.weapon = weapon;
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+
+        weapon.Enter();
     }
 }
---
+++ Pixel Hero - Unity/Assets/_Script/Weapons/Weapon.cs
@@ -0,0 +1,25 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+namespace Damoeson.Weapons
+{
+    public class Weapon : MonoBehaviour
+    {
+        private Animator anim;
+        private GameObject baseGameObject;
+
+        public void Enter()
+        {
+            print($"{transform.name} enter");
+
+            anim.SetBool("active", true);
+        }
+
+        private void Awake()
+        {
+            baseGameObject = transform.Find("Base").gameObject;
+            anim = baseGameObject.GetComponent<Animator>();
+        }
+    }
+}
