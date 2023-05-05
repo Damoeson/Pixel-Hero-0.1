65c76e39ba81cb3a1997f0b56659a53f223ed786
Damoeson <131646917+Damoeson@users.noreply.github.com>
2023-05-05 14:01:33 +0200

    Update WeaponSprite.cs

--- Pixel Hero - Unity/Assets/_Script/Weapons/Components/WeaponSprite.cs
+++ Pixel Hero - Unity/Assets/_Script/Weapons/Components/WeaponSprite.cs
@@ -1,9 +1,6 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-namespace Damoeson
+namespace Damoeson.Weapons.Components
 {
     public class WeaponSprite : WeaponComponent
     {
